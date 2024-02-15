7ef26ee4c4de28e51312fb602d093cba5b15a492
Podolianyk <147654168+Podolianyk@users.noreply.github.com>
2024-02-15 08:05:11 +0100

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,66 @@
+// Задача 2. Користувачі з другом
+// Стрілочна функція getUsersWithFriend(users, friendName) приймає два параметра:
+// перший параметр users — масив об'єктів користувачів
+// другий параметр friendName — ім'я друга для пошуку.
+// до параметру users застосовуємо метод filter, який перебирає усіх user масиву users
+// і якщо user має властивість friends зі значенням параметру frienName, то ім'я user додається до нового масиву
+
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName))
+
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
